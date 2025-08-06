ab24a4d462d6a2b7ac7215705cc773516d51a022
Nguyen Trung Khanh <nguyentrungkhanh97@gmail.com>
2025-08-06 21:19:26 +0700

    chore: rename

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub use http_body_util;
 pub use hyper;
 pub use hyper_tungstenite::tungstenite;
 pub use hyper_util;
+use mimalloc::MiMalloc;
 
 mod certificate;
 mod proxy;
@@ -14,4 +15,7 @@ pub mod builder;
 pub mod handler;
 pub mod utils;
 
-pub use builder::Proxy as Tempo;
+pub use builder::Tempo;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
